63b475147ac13d2bfdacc6235ae84f47c721aae0
barbara-tshimologong <barbara@tshimologong.joburg>
2022-08-18 11:21:10 +0200

    Add project files.

---
+++ Exploring.Static/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Exploring.Static
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, Please type your name");
+            var personName = Console.ReadLine();
+
+            Console.WriteLine($"Good Morning {personName}, we are about to explore ......Static!!!!!");
+        }
+    }
+}
---
+++ Exploring.Static/StaffManagement.cs
@@ -0,0 +1,17 @@
+﻿namespace Exploring.Static
+{
+
+    static class StaffManagement
+    {
+
+        public static int NoOfEmployees { get; set; }
+        public static void AddEmployee()
+        {
+            NoOfEmployees = +1;
+        }
+        public static void RemoveEmployee()
+        {
+            NoOfEmployees = -1;
+        }
+    }
+}
